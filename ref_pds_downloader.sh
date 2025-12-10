f19dac454e10292686eab45d9ae76edfb49ea313
Lewis <did:plc:3fwecdnvtcscjnrx2p4n7alz>
2025-12-10 20:04:52 +0200

    More ref PDSes

--- ref_pds_downloader.sh
+++ ref_pds_downloader.sh
@@ -1,5 +1,19 @@
-git clone --depth 1 https://github.com/haileyok/cocoon reference-pds
+#!/bin/bash
 
-cd reference-pds
+echo "Downloading haileyok/cocoon"
+git clone --depth 1 https://github.com/haileyok/cocoon reference-pds-hailey
+rm -rf reference-pds-hailey/.git
 
-rm -rf .git
+echo "Downloading bluesky-social/atproto pds package"
+mkdir reference-pds-bsky
+cd reference-pds-bsky
+git init
+git remote add origin https://github.com/bluesky-social/atproto.git
+git config core.sparseCheckout true
+echo "packages/pds" >> .git/info/sparse-checkout
+git pull --depth 1 origin main
+mv packages/pds/* .
+rm -rf packages .git
+cd ..
+
+echo "Downloads complete!"
