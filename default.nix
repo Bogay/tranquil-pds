770a16b043d2bd5c5a5af07d97a828d026540008
teq <teqed@shatteredsky.net>
2026-03-22 18:57:16 -0400

    fix(nix): add hashes for git deps

--- default.nix
+++ default.nix
@@ -32,7 +32,16 @@ in rustPlatform.buildRustPackage {
     openssl
   ];
 
-  cargoLock.lockFile = ./Cargo.lock;
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "curve25519-dalek-4.1.3" = "1013mg8xna6jjgq9kvpy0dlbhh5zv1wll9kw1fyplrhw91w7py3c";
+      "libsignal-protocol-0.1.0" = "1dk71757dccvyknn106kkf19a5arlkyinrb6x8xh2ly6f717jick";
+      "spqr-1.4.0" = "0hx2nmkscv7pn5qan86g8k9ljlvp7dshbspqivsqv0wgsydlcdns";
+      "libsignal-service-0.1.0" = "0xxr0ipl0pl35m3518fa8sjnp8viwsni41xv6s0f3mar45h3wydf";
+      "presage-0.8.0-dev" = "16hrnjikpz5dqwa9v74akn4l0xkb64ld7qkpq3rw067bn05cjxis";
+    };
+  };
 
   doCheck = false;
 
